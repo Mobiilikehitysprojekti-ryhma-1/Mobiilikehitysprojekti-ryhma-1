b307f70c33ede49d9703ea8290ca8e5ee68e71a7
Rieskamies <NLappalainen@protonmail.com>
2026-02-08 14:37:29 +0200

    Added a screen wrapper, app backround and a fix for SecondaryButton

---
+++ src/shared/components/ScreenWrapper.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, TouchableWithoutFeedback, Keyboard, ViewStyle } from 'react-native';
+import { useAppTheme } from "../../shared/theme/theme";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+interface ScreenWrapperProps {
+    children: React.ReactNode;
+    style?: ViewStyle;
+}
+
+export const ScreenWrapper = ({ children, style }: ScreenWrapperProps) => {
+    const { spacing, colors } = useAppTheme();
+
+    return (
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <ImageBackground
+                    source={require('../assets/background.jpg')}
+                    style={styles.background}
+                    resizeMode="cover">
+                    <View //Comes to every screen when using <ScreenWrapper>, think as "universal view"
+                        style={[{
+                            flex: 1,
+                            padding: spacing.large,
+                            backgroundColor: 'rgba(70, 130, 180, 0.4)', // fallback
+                        },
+                            style //Allows passing stuff from single screens for specific style options.
+                        ]}
+                    >
+                        {children /*Renders everything and anything else that's put inside <ScreenWrapper>*/}
+                    </View>
+                </ImageBackground>
+            </TouchableWithoutFeedback>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+    },
+});
