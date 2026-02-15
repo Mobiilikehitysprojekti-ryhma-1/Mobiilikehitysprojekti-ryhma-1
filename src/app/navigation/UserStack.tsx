ebd1a9c43cc0f7bd0989398734a7ad82a680d171
Rieskamies <NLappalainen@protonmail.com>
2026-02-15 15:17:37 +0200

    UI fix for user GPS screen

--- src/app/navigation/UserStack.tsx
+++ src/app/navigation/UserStack.tsx
@@ -4,6 +4,7 @@ import type { UserStackParamList } from "../../features/user/navigation/types";
 import { UserHomeStack } from "../../features/user/navigation/UserHomeStack";
 import MeasurementsScreen from "../../features/user/screens/MeasurementsScreen";
 import SafetyScreen from "../../features/user/screens/SafetyScreen";
+import { Icon } from "react-native-paper";
 
 
 const Tab = createBottomTabNavigator<UserStackParamList>();
@@ -13,7 +14,13 @@ export function UserStack() {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={UserHomeStack} options={{ title: "Home", headerShown: false }} />
       <Tab.Screen name="Measurements" component={MeasurementsScreen} options={{ title: "Camera" }} />
-      <Tab.Screen name="Safety" component={SafetyScreen} options={{ title: "GPS" }} />
+      <Tab.Screen name="Safety" component={SafetyScreen} options={{
+        title: "GPS",
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Icon source="map-marker" color={color} size={size} />
+        ),
+      }} />
     </Tab.Navigator>
   );
 }
